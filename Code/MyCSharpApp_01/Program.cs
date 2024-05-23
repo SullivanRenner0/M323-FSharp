4f68f727f15ff14f3bc814eaee12b67b2eb889c1
Sullivan Renner <renner.sullivan@gmail.com>
2024-05-23 16:53:29 +0200

    CSharp Project

---
+++ Code/MyCSharpApp_01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MyCSharpApp_01;
+
+using MyFSharpLib_01;
+
+internal class Program
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var three = MyLibrary_01.add(1, 2);
+        Console.WriteLine("1 + 2 = " + three);
+
+        var minusOne = MyLibrary_01.sub(1, 3);
+        Console.WriteLine("1 - 3 = " + minusOne);
+
+        var six = MyLibrary_01.multiply(2, 3);
+        Console.WriteLine("2 * 3 = " + six);
+
+        var four = MyLibrary_01.divide(8, 2);
+        Console.WriteLine("8 / 2 = " + four);
+
+        try
+        {
+            MyLibrary_01.divide(2, 0);
+            Console.WriteLine("This line will not be printed.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("DivideByZeroException thrown");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception thrown:" + e.Message);
+        }
+
+        return 0;
+    }
+}
--- Code/MyFSharpApps/Program.fs
+++ Code/MyFSharpApps/Program.fs
@@ -1,2 +1,40 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+open System;
+open MyFSharpLib_01;
+
+let printSomeText() = 
+  let text = "Hello World"
+  printfn "text = %s" text
+
+let showCommandLineArgs() = 
+  for arg in System.Environment.GetCommandLineArgs() do
+    printfn "arg = %s" arg
+
+[<EntryPoint>]
+let main args = 
+  //printfn "Arguments: %A" args
+
+  //printSomeText()
+  //showCommandLineArgs()
+
+  let three = MyLibrary_01.add 1.0 2.0
+  printfn "1.0 + 2.0 = %f" three
+
+  let minustwo = MyLibrary_01.sub 1.0 3.0
+  printfn "1.0 - 3.0 = %f" minustwo
+
+  let six = MyLibrary_01.multiply 2.0 3.0
+  printfn "2.0 * 3.0 = %f" six
+
+  let four = MyLibrary_01.divide 8.0 2.0
+  printfn "8.0 / 2.0 = %f" four
+
+  try
+    let result = MyLibrary_01.divide 2.0 0.0
+    printfn "This will no be printed"
+  with
+  | :? System.DivideByZeroException -> printfn "DivideByZeroException thrown"
+  | x -> printfn "Exception thrown: %s" x.Message
+
+  0 // return an integer exit code
